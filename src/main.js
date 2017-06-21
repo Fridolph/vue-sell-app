9abc7656f84a81936c5fda2df41a811c8bac41b2
fridolph <249121486@qq.com>
2017-06-21 16:54:19 +0800

    sell app 项目代码~

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+// 注册第三方组件
+Vue.use(VueResource)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
